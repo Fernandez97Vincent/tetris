c99d7854976f4aa8fa4a4a6244c86d41733054b7
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-09-28 21:42:51 -0700

    Created the tetrominos as well as started them at the center of the page randomly

--- script.js
+++ script.js
@@ -5,8 +5,61 @@ const score = document.getElementById('score');
 // width
 const width = 10;
 
-showAlert = () => {
-    alert('HI!');
-} 
+// creating shapes of tetrominos
+const tetrominoL = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
 
-startBtn.addEventListener('click', showAlert);
+  const tetrominoZ = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const tetrominoT = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+
+  const tetrominoO = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const tetrominoI = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
+  const allTetrominos = [tetrominoL, tetrominoZ, tetrominoT, tetrominoO, tetrominoI];
+
+  let currentPosition = 4;
+  let currentRotation = 0;
+  // select tetrominos randomly
+  const randomTetromino = Math.floor(Math.random()*allTetrominos.length);
+  console.log(randomTetromino);
+  let current = allTetrominos[random][currentRotation];
+
+  // add clast list tetromino to all 
+  function draw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.add('tetromino');
+    })
+  }
+
+  function undraw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.remove('tetromino');
+    })
+  }
+
